b9f730d4161cb8f8cd9d7156f773c55bcd293ac4
GoodmanBen <goodmanben@gmail.com>
2023-06-29 12:22:03 -0400

    fix: data processing structure for GCS w/corrected unit test

--- main/internal/implementations/terraform_workspace/search_repo_workspaces_test.go
+++ main/internal/implementations/terraform_workspace/search_repo_workspaces_test.go
@@ -2,7 +2,6 @@ package terraformWorkspace
 
 import (
 	"context"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -54,11 +53,10 @@ func TestTerraformCloud_cleanDirectoryName(t *testing.T) {
 	}
 }
 
-func TestExtractBackendDetails(t *testing.T) {
+func TestExtractBackendDetails_GCS(t *testing.T) {
 	// Given
 	ctx := context.Background()
 
-	filePath := "test.tfstate"
 	fileContent := []byte(`
 			terraform {
 			  backend "gcs" {
@@ -67,21 +65,91 @@ func TestExtractBackendDetails(t *testing.T) {
 			  }
 			}
 		`)
-	err := os.WriteFile(filePath, fileContent, 0600)
-	require.NoError(t, err)
 
 	// When
 	backendType := "gcs"
-	backendDetails, err := extractBackendDetails(ctx, filePath, fileContent, backendType)
+	backend, err := extractBackendDetails(ctx, fileContent, backendType)
 
 	// Then
 	require.NoError(t, err, "Unexpected error: %v", err)
 
-	gcsBackend, ok := backendDetails.(BackendGCS)
+	gcsBackend, ok := backend.(GCSBackendBlock)
 
 	assert.True(t, ok, "Failed to cast backendDetails to BackendGCS")
 	assert.NotEmpty(t, gcsBackend.Bucket, "Failed to extract backend bucket for GCS")
 	assert.NotEmpty(t, gcsBackend.Prefix, "Failed to extract backend prefix for GCS")
+
+	require.Equal(t, "my-gcs-terraform-backend-bucket", gcsBackend.Bucket, "Failed to extract backend bucket for GCS")
+	require.Equal(t, "terraform/state", gcsBackend.Prefix, "Failed to extract backend prefix for GCS")
+}
+
+func TestExtractBackendDetails_S3(t *testing.T) {
+	// Given
+	ctx := context.Background()
+
+	fileContent := []byte(`
+			terraform {
+			  backend "s3" {
+				bucket = "state-management-bucket"
+				key    = "files/test.tfstate"
+				region = "us-east-1"
+			  }
+			}
+		`)
+
+	// When
+	backendType := "s3"
+	backend, err := extractBackendDetails(ctx, fileContent, backendType)
+
+	// Then
+	require.NoError(t, err, "Unexpected error: %v", err)
+
+	s3BackendBlock, ok := backend.(S3BackendBlock)
+
+	assert.True(t, ok, "Failed to cast backendDetails to BackendGCS")
+	assert.NotEmpty(t, s3BackendBlock.Bucket, "Failed to extract backend bucket for S3")
+	assert.NotEmpty(t, s3BackendBlock.Key, "Failed to extract backend key for S3")
+	assert.NotEmpty(t, s3BackendBlock.Region, "Failed to extract backend region for S3")
+
+	require.Equal(t, "state-management-bucket", s3BackendBlock.Bucket)
+	require.Equal(t, "files/test.tfstate", s3BackendBlock.Key)
+	require.Equal(t, "us-east-1", s3BackendBlock.Region)
+}
+
+func TestExtractBackendDetails_Azurerm(t *testing.T) {
+	// Given
+	ctx := context.Background()
+
+	fileContent := []byte(`
+			terraform {
+			  backend "azurerm" {
+				resource_group_name  = "dragondrop-dev"
+				storage_account_name = "dragondropstoragedev"
+				container_name       = "state-files-container"
+				key                  = "test-state.terraform.tfstate"
+			  }
+			}
+		`)
+
+	// When
+	backendType := "azurerm"
+	backend, err := extractBackendDetails(ctx, fileContent, backendType)
+
+	// Then
+	require.NoError(t, err, "Unexpected error: %v", err)
+
+	azureBackendBlock, ok := backend.(AzureBackendBlock)
+
+	assert.True(t, ok, "Failed to cast backendDetails to BackendGCS")
+	assert.NotEmpty(t, azureBackendBlock.ResourceGroupName, "Failed to extract backend resource group name for azurerm")
+	assert.NotEmpty(t, azureBackendBlock.StorageAccountName, "Failed to extract backend storge account name for azurerm")
+	assert.NotEmpty(t, azureBackendBlock.ContainerName, "Failed to extract backend container name for azurerm")
+	assert.NotEmpty(t, azureBackendBlock.Key, "Failed to extract backend key for azurerm")
+
+	require.Equal(t, "dragondrop-dev", azureBackendBlock.ResourceGroupName, "Failed to extract backend resource group name for azurerm")
+	require.Equal(t, "dragondropstoragedev", azureBackendBlock.StorageAccountName, "Failed to extract backend storge account name for azurerm")
+	require.Equal(t, "state-files-container", azureBackendBlock.ContainerName, "Failed to extract backend container name for azurerm")
+	require.Equal(t, "test-state.terraform.tfstate", azureBackendBlock.Key, "Failed to extract backend key for azurerm")
 }
 
 func TestExtractTFCloudWorkspaceNameIfExists(t *testing.T) {
@@ -90,21 +158,18 @@ func TestExtractTFCloudWorkspaceNameIfExists(t *testing.T) {
 
 	tests := []struct {
 		name        string
-		filePath    string
 		fileContent []byte
 		want        string
 		wantErr     bool
 	}{
 		{
 			name:        "Valid case",
-			filePath:    "valid.tf",
 			fileContent: []byte(`cloud { workspaces { name = "test" } }`),
 			want:        "test",
 			wantErr:     false,
 		},
 		{
 			name:        "Invalid case",
-			filePath:    "invalid.tf",
 			fileContent: []byte(`invalid`),
 			want:        "",
 			wantErr:     true,
@@ -114,7 +179,7 @@ func TestExtractTFCloudWorkspaceNameIfExists(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			// When
-			got, err := extractTFCloudWorkspaceNameIfExists(ctx, tt.filePath, tt.fileContent)
+			got, err := extractTFCloudWorkspaceNameIfExists(ctx, tt.fileContent)
 
 			// Then
 			if tt.wantErr {
