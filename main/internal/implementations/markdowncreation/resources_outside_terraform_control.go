18bdb7be60e32c93794ff752e00c56c3ea08b444
Alejandro Ortiz <pablo.alejandro@dragondrop.cloud>
2023-09-18 13:57:54 -0600

    fix: add new resources table costs

--- main/internal/implementations/markdowncreation/resources_outside_terraform_control.go
+++ main/internal/implementations/markdowncreation/resources_outside_terraform_control.go
@@ -16,6 +16,101 @@ func (m *MarkdownCreator) setResourcesOutsideOfTerraformControlData(report *doc.
 		return
 	}
 
+	if len(m.costEstimates) == 0 {
+		m.resourcesWithoutCostEstimates(report)
+		return
+	}
+
+	resourcesDetailsByType := m.getCostsEstimations()
+	if len(resourcesDetailsByType) == 0 || !m.atLeastOneValidResource(resourcesDetailsByType) {
+		m.resourcesWithoutCostEstimates(report)
+		return
+	}
+
+	m.resourcesWithCostEstimates(report, resourcesDetailsByType)
+}
+
+// ResourceCostEstimate represents the cost estimate for a resource
+type ResourceCostEstimate struct {
+	ResourceCount  int
+	CostComponents map[string]bool
+	IsUsageBased   bool
+	MonthlyCost    float64
+}
+
+func (m *MarkdownCreator) getCostsEstimations() map[string]*ResourceCostEstimate {
+	resourcesDetailsByType := make(map[string]*ResourceCostEstimate)
+	for resourceID := range m.newResources {
+		resourceType := strings.Split(resourceID, ".")[0]
+		if _, ok := resourcesDetailsByType[resourceType]; !ok {
+			resourcesDetailsByType[resourceType] = &ResourceCostEstimate{}
+		}
+
+		resourcesDetailsByType[resourceType].ResourceCount++
+	}
+
+	for _, costEstimate := range m.costEstimates {
+		resourceType := strings.Split(costEstimate.ResourceName, ".")[0]
+
+		if _, ok := resourcesDetailsByType[resourceType]; ok {
+			if resourcesDetailsByType[resourceType].CostComponents == nil {
+				resourcesDetailsByType[resourceType].CostComponents = make(map[string]bool)
+			}
+
+			resourcesDetailsByType[resourceType].CostComponents[costEstimate.CostComponent] = true
+			resourcesDetailsByType[resourceType].IsUsageBased = costEstimate.IsUsageBased
+
+			resourcesDetailsByType[resourceType].MonthlyCost += m.getActualMonthlyCostEstimate(costEstimate)
+		}
+	}
+
+	return resourcesDetailsByType
+}
+
+func (m *MarkdownCreator) atLeastOneValidResource(resourcesByType map[string]*ResourceCostEstimate) bool {
+	validResourcesByType := make(map[string]*ResourceCostEstimate)
+
+	for resourceType, resourceDetail := range resourcesByType {
+		if len(resourceDetail.CostComponents) > 0 && ((resourceDetail.MonthlyCost > 0) || (resourceDetail.MonthlyCost == 0 && resourceDetail.IsUsageBased)) {
+			validResourcesByType[resourceType] = resourceDetail
+		}
+	}
+
+	return len(validResourcesByType) > 0
+}
+
+// resourcesWithCostEstimates sets the resources outside terraform control data in the markdown report
+func (m *MarkdownCreator) resourcesWithCostEstimates(report *doc.MarkDownDoc, resourcesDetailsByType map[string]*ResourceCostEstimate) {
+	report.Write("|Type|# Resources|Cost Components|Monthly Cost|Usage Based*|").Writeln()
+	report.Write("| :---: | :---: | :---: | :---: | :---: |").Writeln()
+
+	for resourceType, resourceDetail := range resourcesDetailsByType {
+		report.Write(fmt.Sprintf("|%s", resourceType))
+		report.Write(fmt.Sprintf("|%d", resourceDetail.ResourceCount))
+
+		if len(resourceDetail.CostComponents) > 0 {
+			report.Write(fmt.Sprintf("|%d", len(resourceDetail.CostComponents)))
+			if resourceDetail.IsUsageBased {
+				report.Write("|$0.00*")
+				report.Write("|True|")
+			} else {
+				report.Write(fmt.Sprintf("|$%.2f", resourceDetail.MonthlyCost))
+				report.Write("|False|")
+			}
+		} else {
+			report.Write("|No Charge")
+			report.Write("|No Charge")
+			report.Write("|No Charge|")
+		}
+
+		report.Writeln()
+	}
+
+	report.Writeln()
+}
+
+// resourcesWithoutCostEstimates sets the resources outside terraform control data in the markdown report
+func (m *MarkdownCreator) resourcesWithoutCostEstimates(report *doc.MarkDownDoc) {
 	resourcesCounterByType := make(map[string]int)
 	for resourceID := range m.newResources {
 		resourceType := strings.Split(resourceID, ".")[0]
